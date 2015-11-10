dcab298de0ae00114758c2bc91442c4ef7cdc65b
Cabezas <javiergonzalezcabezas@gmail.com>
2015-11-10 20:08:21 +0000

    added CloudStorage

--- src/main/java/com/thedeveloperworldisyours/cloudstorage/MainActivity.java
+++ src/main/java/com/thedeveloperworldisyours/cloudstorage/MainActivity.java
@@ -1,13 +1,51 @@
 package com.thedeveloperworldisyours.cloudstorage;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.thedeveloperworldisyours.cloudstorage.utils.CloudStorage;
+import com.thedeveloperworldisyours.cloudstorage.utils.Work;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void uploadClick(View view) {
+        Message message = new Message();
+        message.what = Work.UPLOAD_FILE;
+        handler.sendMessage(message);
+    }
+
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Work.UPLOAD_FILE:
+                    upload();
+                    break;
+
+            }
+        }
+    };
+
+    public void upload() {
+        try {
+            CloudStorage.uploadFile("bucket-xxx", "photo.jpg");
+
+        } catch (Exception e) {
+            Log.d(TAG, "Exception: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/com/thedeveloperworldisyours/cloudstorage/utils/Work.java
@@ -0,0 +1,8 @@
+package com.thedeveloperworldisyours.cloudstorage.utils;
+
+/**
+ * Created by javiergonzalezcabezas on 10/11/15.
+ */
+public class Work {
+    public static final int UPLOAD_FILE = 0;
+}
